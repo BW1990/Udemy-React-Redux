4971b181f45796368fb687b5c136efee93d36258
Brian <brianwinward@hotmail.com>
2017-09-13 16:14:01 -0400

    Weather Map App

---
+++ weather/src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const API_KEY = "7f2a272b6eb3d4aca51a78e6d63b0248";
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = "FETCH_WEATHER";
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
